c1c132ee0bae13cbc1e6ca8d055c37cd9b26666f
Koi <62371137+KaoriAnimu@users.noreply.github.com>
2022-07-06 19:31:55 +0700

    Delete api directory

--- api/src/main/java/com/example/tubes/api/repository/karyawanRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.tubes.api.repository;
-
-import com.example.tubes.api.model.karyawan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface karyawanRepository extends JpaRepository<karyawan, Integer>{
-    
-}
